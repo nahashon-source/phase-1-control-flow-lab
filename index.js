23132bede992c785aec74ef8166c5145f27101aa
nahashon <nashonmwendwa0@gmail.com>
2024-07-12 20:42:04 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+//function scuberGreetingForFeet(){
   // Write your code here!
+//}
+
+f//unction ternaryCheckCity(){
+  // Write your code here!
+//}
+
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return "This one is on me!";
+  } else if (rideDistance > 400 && rideDistance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if(tip=== 'generous'){
+  return 'Thank you so much.'
+}
+else if(tip == 'not as generous'){
+  return "Thank you."
+}else {
+    return "Bye.";
+  }
 }
